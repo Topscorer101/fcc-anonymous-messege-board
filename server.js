8e6283259417d20390495b29b89e31f57b971cd9
Topscorer101 <46632281+Topscorer101@users.noreply.github.com>
2019-10-14 20:45:21 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,69 @@
+'use strict';
+
+require('dotenv').config();
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var expect      = require('chai').expect;
+var cors        = require('cors');
+const helmet = require("helmet");
+
+var apiRoutes         = require('./routes/api.js');
+var fccTestingRoutes  = require('./routes/fcctesting.js');
+var runner            = require('./test-runner');
+const { mongoose } = require("./db/mongoose");
+
+var app = express();
+
+app.use(helmet());
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.route('/b/:board/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/board.html');
+  });
+app.route('/b/:board/:threadid')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/thread.html');
+  });
+
+
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+  
+fccTestingRoutes(app);
+
+apiRoutes(app);
+
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        var error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
+      }
+    }, 1500);
+  }
+});
+
+module.exports = app; //for testing
